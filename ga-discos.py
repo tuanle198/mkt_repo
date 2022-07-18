9afeee45e6cf3947d25aefb719ed4ff311c75566
unknown <legiatuan1998@gmail.com>
2022-07-18 16:24:26 +0700

    change color and add map plot

--- ga-discos.py
+++ ga-discos.py
@@ -8,6 +8,7 @@ from apiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
 import base64
 
+
 st.set_page_config(
     page_title="Real-Time DISCOS App Listing Dashboard",
     page_icon="✅",
@@ -110,6 +111,7 @@ def int_converter(column):
 
 
 body = {'reportRequests': [{'viewId': '266420819',
+                            'pageSize': 10000,
                             'dateRanges': [{'startDate': '2022-05-01', 'endDate': 'today'}],
                             'metrics': [{'expression': 'ga:pageviews'},
                                         {'expression': 'ga:users'},
@@ -122,19 +124,20 @@ body = {'reportRequests': [{'viewId': '266420819',
                                            {'name': 'ga:date'},
                                            {'name': 'ga:source'},
                                            {'name': 'ga:country'},
+                                           {'name': 'ga:countryIsoCode'},
                                            {'name': 'ga:deviceCategory'}]}]}
 
 df = run_report(body, KEY_FILE_LOCATION)
-
+iso_convert = pd.read_csv('iso_convert.csv')
 # Convert unexpected float columns to int
-df.columns = ['Landing Page', 'Date', 'Source', 'Country', 'Device', 'Pageviews', 'Users', 'New Users',
+df.columns = ['Landing Page', 'Date', 'Source', 'Country', 'ISO', 'Device', 'Pageviews', 'Users', 'New Users',
               'Sessions', 'Bounce Rate', 'Avg. Session Duration', 'Total Events']
 int_columns = ['Pageviews', 'Users', 'New Users',
                'Sessions', 'Bounce Rate', 'Total Events']
 for column in int_columns:
     int_converter(column)
 
-# Convert date column to date dtype
+    # Convert date column to date dtype
 df['Date'] = pd.to_datetime(df['Date'], format='%Y%m%d')
 
 # Extract type of surface
@@ -145,7 +148,6 @@ df['type'] = df['Landing Page'].apply(lambda x: x.split(
 df['keyword'] = df['Landing Page'].apply(lambda x: x.split('surface_detail=')[1].split(
     '&')[0] if 'surface_detail' and 'surface_type=search' in x else 'Others')
 df['keyword'] = df['keyword'].apply(lambda x: x.replace('+', ' '))
-
 # Setup dashboard layout
 st.title("Real-Time DISCOS App Listing Dashboard")
 
@@ -164,13 +166,14 @@ with date_col_end:
         date.today(),
         min_value=sd)
 
-# Exclude vietnam option
+    # Exclude vietnam option
 exclude = st.radio(
     "Exclude Vietnam",
     ('Yes', 'No'))
 
 # Filter by date
-date_mask = (df['Date'] >= pd.Timestamp(sd)) & (df['Date'] <= pd.Timestamp(ed))
+date_mask = (df['Date'] >= pd.Timestamp(sd)) & (
+    df['Date'] <= pd.Timestamp(ed))
 df = df[date_mask]
 
 # Exclude vietnam
@@ -178,17 +181,15 @@ country_mask = df['Country'] != 'Vietnam'
 if exclude == 'Yes':
     df = df[country_mask]
 
-# Create dataframe for fig_col1
-main = ['upsell cross sell', 'bogo', 'cart upsell']
-desc = ['increase sales',
+    # Define main and desc keywords
+main = ['buy x get y', 'bogo', 'flash sale', 'flash sales', 'discount']
+desc = ['cart upsell',
         'promotional campaign',
-        'buy one get one',
-        'buy x get y',
+        'buy one get one'
         'in cart upsell',
-        'one click upsell',
         'upsell']
 
-# Create dataframe for fig_col2 and fig_col3 and fig_col6
+# Create dataframe for fig_col2 and fig_col3 and fig_col4
 keyword_group = df[df['keyword'] != 'Others'].groupby('keyword').agg(
     {'Pageviews': 'sum',
      'Avg. Session Duration': 'mean',
@@ -206,18 +207,25 @@ agg_filtered = df.groupby('Date').agg(
     {'Pageviews': 'sum', 'Total Events': 'sum'}).reset_index()
 agg_filtered['Date'] = agg_filtered['Date'].apply(lambda x: x.date())
 
-# Create dataframe for fig_col4
-countries = df.groupby('Country')['Pageviews'].sum().reset_index(
-).sort_values('Pageviews', ascending=False).reset_index()
-countries.loc[countries['Pageviews'] <
-              countries['Pageviews'][4], 'Country'] = 'Others'
-countries = countries.groupby('Country')['Pageviews'].sum(
-).sort_values(ascending=False).reset_index()
+# Create dataframe fig_col4
+click_df = df.groupby('keyword')['Total Events'].sum().reset_index()
+click_df['Target Priority'] = 'None'
+click_df.loc[click_df['keyword'].apply(
+    lambda x: x in desc), 'Target Priority'] = 'Include in desc.'
+click_df.loc[click_df['keyword'].apply(
+    lambda x: x in main), 'Target Priority'] = 'Main'
+click_df = click_df.sort_values(
+    'Total Events', ascending=False).head(10)
 
 # Create dataframe for fig_col5
 devices = df.groupby('Device')['Pageviews'].sum(
 ).sort_values(ascending=False).reset_index()
 
+# Create dataframe for fig_col6
+countries = df.groupby(['Country', 'ISO'])['Pageviews'].sum(
+).sort_values(ascending=False).reset_index()
+countries = countries.merge(iso_convert, how='left',
+                            left_on='ISO', right_on='ISO-2')
 # create three columns to store all source
 all_kpi1, all_kpi2, all_kpi3, all_kpi4 = st.columns(4)
 
@@ -251,15 +259,17 @@ all_kpi4.metric(
 # Chart section
 fig_col1, fig_col2 = st.columns(2)
 with fig_col1:
-    st.markdown("### Traffic by days")
+    st.markdown("### Traffic and click add apps by days")
     fig1 = px.line(data_frame=agg_filtered, x="Date",
-                   y="Pageviews", markers=True)
+                   y=["Pageviews", 'Total Events'], color_discrete_sequence=["#008060", '#ffc453'])
     # fig1.add_bar(data_frame=agg_filtered, x="Date", y="Total Events")
     st.write(fig1)
 with fig_col2:
     st.markdown("### Top 10 keywords by traffic")
     fig2 = px.bar(data_frame=top_keyword_group, x="keyword",
-                  y="Pageviews", color='Target Priority')
+                  y="Pageviews", color='Target Priority', color_discrete_sequence=["#008060", '#ffc453', '#000000'])
+    fig2.update_layout(barmode='stack', xaxis={
+        'categoryorder': 'total descending'})
     st.write(fig2)
 
 fig_col3, fig_col4 = st.columns(2)
@@ -269,26 +279,38 @@ with fig_col3:
     fig3 = px.bar(data_frame=top_keyword_group, x="keyword",
                   y="Avg. Session Duration", color='Target Priority',
                   labels={
-                      "Avg. Session Duration": "Avg. Session Duration (seconds)"
-                  }
+                    "Avg. Session Duration": "Avg. Session Duration (seconds)"
+                  }, color_discrete_sequence=["#008060", '#ffc453', '#000000']
                   )
+    fig3.update_layout(barmode='stack', xaxis={
+        'categoryorder': 'total descending'})
     st.write(fig3)
 with fig_col4:
-    st.markdown("### Top 5 countries with highest traffic")
-    fig4 = px.pie(data_frame=countries, names="Country", values='Pageviews')
+    st.markdown("### Top 10 keywords by click add app")
+    fig4 = px.bar(data_frame=top_keyword_group, x="keyword",
+                  y="Total Events", color='Target Priority',
+                  color_discrete_sequence=["#008060", '#ffc453', '#000000'])
+    fig4.update_layout(barmode='stack', xaxis={
+        'categoryorder': 'total descending'})
     st.write(fig4)
 
 fig_col5, fig_col6 = st.columns(2)
 with fig_col5:
     st.markdown("### User distribution by device")
-    fig5 = px.pie(data_frame=devices, names="Device", values='Pageviews')
+    fig5 = px.pie(data_frame=devices, names="Device", values='Pageviews',
+                  color_discrete_sequence=["#008060", '#ffc453', '#000000'])
     st.write(fig5)
 with fig_col6:
-    st.markdown("### Click add app by keyword")
-    fig6 = px.bar(data_frame=top_keyword_group, x="keyword",
-                  y="Total Events", color='Target Priority')
+    st.markdown("### Pageviews by countries")
+    # fig6 = px.pie(data_frame=countries, names="Country", values='Pageviews',
+    #               color_discrete_sequence=["#008060", '#ffc453', '#000000', '#95A3B3', '#7EBCE6', '#B7D5D4'])
+    fig6 = px.choropleth(countries, locations="ISO-3",
+                         color="Pageviews",
+                         hover_name="Country", color_continuous_scale=['#ffffff', '#ffc453', '#008060'])
     st.write(fig6)
+
 # Showing and downloading raw data
+
 st.markdown("### Raw data from GA analytics")
 number = st.number_input(label='Number of rows to be shown',
                          min_value=5,
